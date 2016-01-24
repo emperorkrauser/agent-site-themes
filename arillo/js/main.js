26b0111ee89400ce4f39434a9d90729e83a2ab63
Christian Adam Rosell <krauserchristian@gmail.com>
2016-01-25 00:21:44 +0800

    added slider and initial search form

--- arillo/js/main.js
+++ arillo/js/main.js
@@ -1,3 +1,15 @@
+$(window).ready(function() {
+	'use strict';
+	$(function() {
+	    $('body').vegas({
+	        slides: [
+	            { src: '../arillo/img/bg01.jpg' },
+	            { src: '../arillo/img/bg02.jpg' }
+	        ]
+	    });
+	});
+});
+
 $(document).ready(function(){
 	
 });
