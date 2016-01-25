a5b35d1d991c97e60f13183ed2437847e1332596
Christian Adam Rosell <krauserchristian@gmail.com>
2016-01-26 01:09:46 +0800

    added agent info,contact,blog section, also added property-detail page

--- arillo/js/main.js
+++ arillo/js/main.js
@@ -1,7 +1,7 @@
 $(window).ready(function() {
 	'use strict';
 	$(function() {
-	    $('body').vegas({
+	    $('.slider-area').vegas({
 	        slides: [
 	            { src: '../arillo/img/property1.jpg' },
 	            { src: '../arillo/img/property2.jpg' }
