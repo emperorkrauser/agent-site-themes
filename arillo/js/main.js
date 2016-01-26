8f39c62729462c47d11b1c33a5e6c01698b87380
Christian Adam Rosell <krauserchristian@gmail.com>
2016-01-27 02:10:20 +0800

    added search fields on property-detail-page, initial design for gallery

--- arillo/js/main.js
+++ arillo/js/main.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+	var $galleryLength = $('.photo-gallery .col-md-2').length;
+	var $this = $('.photo-gallery .col-md-2');
+
+	var count=0;
+	$this.each(function(){
+		if(++count == 8){
+        	$this.css('display','block');
+        }
+        else if(count > 8){
+         	$(this).css('display','none');
+        }
+	});
+});
+
 $(window).ready(function() {
 	'use strict';
 	$(function() {
@@ -8,8 +23,4 @@ $(window).ready(function() {
 	        ]
 	    });
 	});
-});
-
-$(document).ready(function(){
-	
 });
